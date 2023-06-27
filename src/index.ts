db0aa0c0379d867513391e6a8406a93ba12c1e53
yusakuvol <yoursakuyu@gmail.com>
2023-06-27 23:07:32 +0900

    update order

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const symbol = "BTC/JPY";
 const subscribeChannel = "parent_order_events";
 let averagePrice = 0;
 let orderCount = 0;
+let sellOrderId = "";
 
 // 初期化処理
 const initialize = async () => {
@@ -35,48 +36,54 @@ const main = async () => {
     const { bitFlyerClient, bitFlyerWebSocket } = await initialize();
 
     try {
-      // 動かした時点での価格を取得
-      // 価格はRESTで取得
-      const { ask, bid } = await bitFlyerClient.getTicker(symbol);
-      const middlePrice = (ask + bid) / 2;
+      // 動かした時点での価格をRESTで取得
+      const { bid } = await bitFlyerClient.getTicker(symbol);
 
       // 買い注文を買い目ごとに8個作成(買い目パーセンテージごとに作成)
-      for (let i = 1; i <= 8; i++) {
+      for (let i = 0; i < 8; i++) {
         // 買い目の価格を計算
         const buyingPrice = Math.floor(
-          middlePrice * (1 - buyingIntervalPercentage / 100) ** i
+          bid * (1 - buyingIntervalPercentage / 100) ** i
         );
 
         // 買い注文を作成
-        //   const order = await bitFlyerClient.createLimitBuyOrder(
-        //     symbol,
-        //     unit,
-        //     buyingPrice
-        //   );
+        await bitFlyerClient.createLimitBuyOrder(symbol, unit, buyingPrice);
         console.log(`create buyOrder price: ${buyingPrice}`);
       }
 
       // 約定を取得
-      bitFlyerWebSocket.subscribe(subscribeChannel, (channel, message) => {
-        // 平均取得価格を計算
-        const price = message.data.price;
-        orderCount++;
-        averagePrice = (averagePrice * (orderCount - 1) + price) / orderCount;
-      });
+      bitFlyerWebSocket.subscribe(
+        subscribeChannel,
+        async (channel, message) => {
+          // 平均取得価格を計算
+          const price = message.data.price;
+          orderCount++;
+          averagePrice = (averagePrice * (orderCount - 1) + price) / orderCount;
 
-      // 売り注文は平均取得価格からprofitLine分上げた価格で指値注文を作成
-      //   const order = await bitFlyerClient.createLimitSellOrder(
-      //     symbol,
-      //     unit,
-      //     profitLine
-      //   );
-      // console.log(`create sellOrder price: ${profitLine}`);
+          // sellOrderがある場合は注文をキャンセル
+          if (sellOrderId === "") {
+            await bitFlyerClient.cancelOrder(sellOrderId);
+          }
+
+          const sellPrice = Math.floor(averagePrice * profitLine);
+          // 売り注文は平均取得価格からprofitLineをかけた価格で作成
+          const order = await bitFlyerClient.createLimitSellOrder(
+            symbol,
+            unit,
+            sellPrice
+          );
+          sellOrderId = order.id;
+          console.log(`create sellOrder price: ${sellPrice}`);
+        }
+      );
     } catch (error) {
       bitFlyerWebSocket.unsubscribe(subscribeChannel);
       console.error(`unhandled error: ${error}`);
+      console.log("Stop bot");
     }
   } catch (error) {
     console.error(`unhandled error: ${error}`);
+    console.log("initialize failed");
   }
 };
 
